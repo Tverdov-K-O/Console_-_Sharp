2a926e12975adda07719e7c2deee5f4f783d830a
tverdov.konstantin tverdov.konstantin <konstantin08081997@mail.ru>
2021-09-22 09:21:36 +0300

    Добавьте файлы проекта.

---
+++ Console_С_Sharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_С_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет!");
+            int[] arr = new int[5] { 1,2,3,4,5};
+            int[] arr2 = new int[] { 1,2,3,4,5};
+            int[] arr3 = new int[5] { 1,2,3,4};
+
+
+            Console.Read();
+        }
+    }
+}
